4a5c41ed01230edd747601271ab39f0d7ad96671
Klementina Pirc <klementina.pirc@student.fmf.uni-lj.si>
2016-11-08 00:03:33 +0100

    V program dodala pripravo za csv datoteko

--- pridobivanje_podatkov.py
+++ pridobivanje_podatkov.py
@@ -1,5 +1,6 @@
 import orodja1
 import re
+import unicodedata
 
 #======================================================================================================================
 #   Pri iskanju oglasov na mobile.de se prikaže le prvih 50 strani, kar bi pomenilo le dobrih 1000 oglasov.
@@ -7,7 +8,7 @@ import re
 #======================================================================================================================
 
 vsa_leta = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016']
-vse_kategorije = ['ChopperAndCruiser', 'EnduroAndTouringEnduro', 'NakedBike',
+vse_kategorije = ['ChopperAndCruiser','EnduroAndTouringEnduro', 'NakedBike',
               'SportsAndSuperSportsBike', 'SportTouringMotorcycle',
               'SuperMoto', 'Tourer']
 
@@ -15,7 +16,7 @@ vse_kategorije = ['ChopperAndCruiser', 'EnduroAndTouringEnduro', 'NakedBike',
 #   Najprej za vsako kategorijo in leto prve registracije shranimo prvo stran oglasov. Tako bomo izvedeli na koliko
 #   straneh so iskani oglasi.
 #======================================================================================================================
-vsi_oglasi = {}
+vsi_oglasi = []
 for kategorija in vse_kategorije:
     for leto in vsa_leta:
         osnovni_naslov = 'http://suchen.mobile.de/fahrzeuge/search.html'
@@ -53,25 +54,70 @@ for kategorija in vse_kategorije:
             orodja1.shrani(nov_naslov, datoteka_1)
 
 #======================================================================================================================
-#   V html datotekah bomo za vsak oglas poiskali id, opis in ceno motocikla,
-
-            # with open(datoteka_1, encoding="utf8") as h:
-            #     vsebina = h.read()
-            #     for oglas in re.finditer(
-            #             r'id=(?P<id>\d{9})'
-            #             r'.+?<span class="h3 u-text-break-word">(?P<motor>.+?)</span>'
-            #             r'.+?<span class="h3 u-block">(?P<cena>.+?)</span>'
-            #             r'.+?class="rbt-regMilPow">(?P<opis>.+?)</div>'
-            #              , vsebina):
-            #
-            #         podatki_oglasa = {}
-            #         id = int(oglas.group('id'))
-            #         podatki_oglasa['motor'] = oglas.group('motor')
-            #         podatki_oglasa['cena'] = oglas.group('cena')
-            #         podatki_oglasa['opis'] = oglas.group('opis')
-            #         vsi_oglasi[id] = podatki_oglasa
-
-#======================================================================================================================
-#   Preden podatke shranimo v csv datoteko, jih moramo še urediti.
+#   V html datotekah bomo za vsak oglas poiskali id oglasa, znamko, starost, moč, prostornino, ceno in prevožene
+#   kilometre motocikla ter jih sočasno še uredili, ter shranili v seznam slovarjev.
 #======================================================================================================================
 
+            with open(datoteka_1, encoding="utf8") as h:
+                vsebina = h.read()
+                for oglas in re.finditer(
+                        r'id=(?P<id>\d{9})'
+                        r'.+?<span class="h3 u-text-break-word">(?P<motor>.+?)</span>'
+                        r'.+?<span class="h3 u-block">(?P<cena>.+?)</span>'
+                        r'.+?class="rbt-regMilPow">(?P<opis>.+?)</div>'
+                         , vsebina):
+
+                    id = int(oglas.group('id'))
+
+                    motor = oglas.group('motor')
+                    motor = motor.split(" ")
+                    if motor[0] == 'Moto' or motor[0] == 'MV':
+                        znamka = motor[0] + ' ' + motor[1]
+                    else:
+                        znamka = motor[0]
+
+                    cena = oglas.group('cena')
+                    cena = cena.strip('€')
+                    cena = cena.strip(' ')
+                    cena = int(cena.replace(".",""))
+
+                    opis = oglas.group('opis')
+                    opis = opis.encode('ascii', 'replace')
+                    opis = opis.decode("utf-8")
+                    opis = opis.replace("?", " ")
+                    stevilo_vejic = opis.count(',')
+                    if stevilo_vejic != 3:
+                        continue
+                    [letnik, km, moc, prostornina] = opis.split(",")
+
+                    km = km.strip('km')
+                    km = km.strip(" ")
+                    km = int(km.replace(".", ""))
+
+                    prostornina = prostornina.split()
+                    prostornina = prostornina[0]
+                    prostornina = int(prostornina.replace(".", ""))
+
+                    letnik = letnik.split("/")
+                    letnik = int(letnik[1])
+
+                    moc = moc.split(" ")
+                    moc = int(moc[1])
+
+                    tip = kategorija
+                    tip = re.findall('[A-Z][a-z]*', tip)
+                    tip = ' '.join(tip)
+
+                    podatki_oglasa = {}
+                    podatki_oglasa['id'] = id
+                    podatki_oglasa['znamka'] = znamka
+                    podatki_oglasa['cena_(€)'] = cena
+                    podatki_oglasa['km'] = km
+                    podatki_oglasa['moc_(kW)'] = moc
+                    podatki_oglasa['letnik'] = letnik
+                    podatki_oglasa['prostornina_(ccm)'] = prostornina
+                    podatki_oglasa['tip'] = tip
+                    vsi_oglasi.append(podatki_oglasa)
+
+orodja1.zapisi_tabelo(vsi_oglasi,['id', 'letnik', 'znamka', 'tip', 'prostornina_(ccm)',
+                                  'moc_(kW)', 'km', 'cena_(€)'], 'motocikli.csv')
